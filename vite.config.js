347d98aec9dd1b9669a2ec45d2dfb7bef8e1e6aa
Johan Yindou <jyindou@gmail.com>
2026-03-25 16:06:16 +0100

    🔧 Update index.html for favicon path and add preconnect link; enhance package.json with Node.js engine specification; configure Vite for optimized build settings and chunking.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    target: 'esnext',
+    minify: 'esbuild', // plus rapide que terser
+    cssMinify: true,
+    
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    }
+  }
 })
